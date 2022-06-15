dad6c8ba89f47061f0cef0c7bbbf1e16c6d95b64
sajib <me.mrsajib@gmail.com>
2022-06-15 16:12:20 +0600

    add to bootstrap cdn

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h2>hey typescript</h2>
+    <div className="container">
+      <h1>hello typescript</h1>
     </div>
   );
 }
