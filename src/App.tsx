7ac55ef02650f49071d3d1a8034d5223a5a24369
sajib <me.mrsajib@gmail.com>
2022-06-15 19:51:13 +0600

    router set

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
+import Update from "./components/Update/Update";
 
 function App() {
   return (
     <>
       <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/user" element={<Home />} />
+        <Route path="/update/:id" element={<Update />} />
+      </Routes>
     </>
   );
 }
